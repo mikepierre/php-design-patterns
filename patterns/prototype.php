c7edf88a55c6859a72d0776b0f155f30f009d000
mikepierre <mikeypierre84@yahoo.com>
2015-10-15 13:06:30 -0400

    Create prototype.php
    the prototype pattern clones an object and while an object is being cloned perofrmance of the current object that is being clone still has effective performance when executing.

---
+++ patterns/prototype.php
@@ -0,0 +1,32 @@
+class Cloner()
+{
+    public $first_obj;
+    public $second_obj;
+
+    function __clone()
+    {
+        $this->second_obj = clone $this->first_obj;
+    }
+}
+
+class Reservations()
+{
+    public static $instances = 0;
+    public  $instance;
+    
+    public function __construct() 
+    {
+        $this->instance = ++self::$instances;
+    }
+
+    public function  __clone() 
+    {
+        $this->instance = ++self::$instances;
+    }
+
+}
+
+$object = new Cloner();
+
+$object->first_obj = new Reservations();
+$object->first_obj = new Reservations();
