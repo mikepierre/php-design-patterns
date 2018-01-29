b5c97b0c1f01346e472e1c7f282c1fcbb235e4b0
mikepierre <mikeypierre84@yahoo.com>
2018-01-29 17:15:10 -0500

    Formatted Code

--- data_structures/stack.php
+++ data_structures/stack.php
@@ -1,43 +1,64 @@
 <?php
-class stack {
-	$the_stack = array();
-	$top = -1;
-	
-	public function push($data = array()) {
-		if(!$this->isFull()) {
-			$top = $this->top + 1;
-			$the_stack[$top] = $data;
-		}
-	}
-	
-	public function pop() {
-		$data = '';
-		if(!$this->isEmpty()) {
-			$data = $this->the_stack[$top];
-			$top = $this->top - 1;
-			return $data;
-		}
-	}
-	
-	public function peek() {
-		return $this->the_stack[$this->top];
-	}
-	
-	public function isFull() {
-		if($this->top == -1) {
-			return -1;	
-		} else {
-			return 0;
-		}
-	}
-	
-	public function isEmpty() {
-		if($this->top == -1) {
-			return 1;
-		} else {
-			return 0;
-		}
-	}
+class stack 
+{
+    public $the_stack = [];
+    public $top = -1;
+
+    /**
+     * @param array $data
+     */
+    public function push(array $data)
+    {
+        if(!$this->isFull()) {
+            $top = $this->top + 1;
+            $the_stack[$top] = $data;
+        }
+    }
+
+    /**
+     * @return mixed|string
+     */
+    public function pop() 
+    {
+        $data = '';
+        if(!$this->isEmpty()) {
+            $data = $this->the_stack[$top];
+            $top = $this->top - 1;
+            return $data;
+        }
+    }
+
+    /**
+     * @return mixed
+     */
+    public function peek() 
+    {
+        return $this->the_stack[$this->top];
+    }
+
+    /**
+     * @return int
+     */
+    public function isFull() 
+    {
+        if($this->top == -1) {
+            return -1;
+        } else {
+            return 0;
+        }
+    }
+
+    /**
+     * @return int
+     */
+    public function isEmpty() 
+    {
+        if($this->top == -1) {
+            return 1;
+        } else {
+            return 0;
+        }
+    }
 }
 
 $stack = new stack();
@@ -46,10 +67,10 @@ $stack->push(1);
 $stack->push(2);
 
 if($stack->isFull() == true) {
-echo 'Stack is full';
+    echo 'Stack is full';
 }
 
 if($stack->isEmpty() == true) {
-echo 'Stack is empty';
+    echo 'Stack is empty';
 }
 ?>
