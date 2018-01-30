cac73dd90cbf46f830874a8b23156c2fd1097cb3
mikepierre <mikeypierre84@yahoo.com>
2018-01-30 13:22:47 -0500

    Refactored

--- data_structures/singleLinkedList.php
+++ data_structures/singleLinkedList.php
@@ -1,52 +1,61 @@
 <?php
-class Node {
-	var $data;
-	var $next;
-	__construct($data, $next){
-		$this->data = $data;
-		$this->next = $next;
-	}
+class Node 
+{
+    var $data;
+    var $next;
+
+    __construct($data, $next)
+    {
+    	$this->data = $data;
+    	$this->next = $next;
+    }
+
 }
 
 
-  class SingleList {
-  	var $head = null;
-  	var $tail = null;
+class SingleList 
+{
+    var $head = null;
+    var $tail = null;
 
-  	public function show() {
-  		$currentNode = $this->head;
-  		while ($currentNode != null) {
-  			echo $currentNode->data;
-  			$currentNode = $currentNode->next;
-  		}
-  	}
+    public function show() 
+    {
+    	$currentNode = $this->head;
+    	while ($currentNode != null) {
+    		echo $currentNode->data;
+    		$currentNode = $currentNode->next;
+    	}
+    }
 
-  	public function append($data) {
-  		$node = new Node($data, null);
-  		if($this->head == null) {
-  			$this->head = $this->tail;
-  			$this->tail = $node;
-  		} else {
-  			$this->tail->next = $node;
-  		}
-  		$this->tail = $node;
-  	}
+    public function append($data) 
+    {
+    	$node = new Node($data, null);
+    	if($this->head == null) {
+    		$this->head = $this->tail;
+    		$this->tail = $node;
+    	} else {
+    		$this->tail->next = $node;
+    	}
+        $this->tail = $node;
+    }
 
-  public function remove($nodeValue) {
-  	$currentNode = $this->head;
-  	$previousNode = null;
-  	while ($currentNode->data != null) {
-  		if($currentNode->data == $nodeValue) {
-  			if($previousNode != null) {
-  				$previousNode->next = $currentNode->next;
-  			} else {
-  				$this->head = $currentNode->next;
-  			}
-  			$previousNode = $currentNode;
-  			$currentNode = $currentNode->next;
-  		}
-  	}
-  }	
+    public function remove($nodeValue) 
+    {
+    	$currentNode = $this->head;
+    	$previousNode = null;
+    	while ($currentNode->data != null) {
+    		if($currentNode->data == $nodeValue) {
+    			if($previousNode != null) {
+    				$previousNode->next = $currentNode->next;
+    			} else {
+    				$this->head = $currentNode->next;
+    			}
+    			$previousNode = $currentNode;
+    			$currentNode = $currentNode->next;
+    		}
+    	}
+    }
+
 }
 
 $s = new SingleList();
